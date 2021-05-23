225ab9d71becebd071b2e99e759decd4fc7b47c2
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-23 21:05:14 +0530

    adding more services for Qualifier example

--- src/main/java/com/shubhajit/spring5didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/shubhajit/spring5didemo/controllers/PropertyInjectedController.java
@@ -1,6 +1,6 @@
 package com.shubhajit.spring5didemo.controllers;
 
-import com.shubhajit.spring5didemo.services.GreetingServiceImpl;
+import com.shubhajit.spring5didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Controller;
 public class PropertyInjectedController {
 
     @Autowired
-    public GreetingServiceImpl greetingService;
+    public GreetingService greetingServiceImpl;
 
     public String sayHello() {
-        return greetingService.sayGreeting();
+        return greetingServiceImpl.sayGreeting();
     }
 }
--- src/main/java/com/shubhajit/spring5didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/shubhajit/spring5didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.shubhajit.spring5didemo.controllers;
 
 import com.shubhajit.spring5didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,7 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("setterGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 }
