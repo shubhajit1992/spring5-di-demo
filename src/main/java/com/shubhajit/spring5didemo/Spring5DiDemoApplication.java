bf40c17848b91eb3b23a887c7c16aad1b294039b
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-23 20:23:47 +0530

    initial commit

---
+++ src/main/java/com/shubhajit/spring5didemo/Spring5DiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shubhajit.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhajit/spring5didemo/Spring5DiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhajit.spring5didemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5DiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
