c4bc9c566fb5905b104963d39af05ab986dcfc1a
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-28 10:23:53 +0530

    adding environment example

--- src/main/java/com/shubhajit/spring5didemo/config/PropertyConfig.java
+++ src/main/java/com/shubhajit/spring5didemo/config/PropertyConfig.java
@@ -1,14 +1,20 @@
 package com.shubhajit.spring5didemo.config;
 
 import com.shubhajit.spring5didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
     @Value("${db.username}")
     String username;
 
@@ -21,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
